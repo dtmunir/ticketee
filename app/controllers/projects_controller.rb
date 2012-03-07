2c99076e7d0d0e599e153df6a2aa8d3782ac58af
J. Venator <j@venator.vc>
2012-03-07 12:17:29 -0500

    Implemented creating tickets for a project - Section 5.1.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -44,11 +44,11 @@ class ProjectsController < ApplicationController
   
   private
   
-  def find_project
-    @project = Project.find(params[:id])
-    rescue ActiveRecord::RecordNotFound
-    flash[:alert] = "The project you were looking" +
-                    " for could not be found."
-    redirect_to projects_path
-end
+    def find_project
+      @project = Project.find(params[:id])
+      rescue ActiveRecord::RecordNotFound
+      flash[:alert] = "The project you were looking" +
+                      " for could not be found."
+      redirect_to projects_path
+    end
 end
